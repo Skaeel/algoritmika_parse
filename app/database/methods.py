219c4f4f3308e14c3280d637a61cd127de8222cb
Skaeel <successdm13@gmail.com>
2025-04-18 20:06:33 +0500

    initial commit

---
+++ app/database/methods.py
@@ -0,0 +1,68 @@
+from .session import engine, session
+from .models import Base, ParserData
+from sqlalchemy import inspect, update, select, delete
+from sqlalchemy.orm import Session
+
+
+def create_tables_if_not_exist():
+    with engine.begin() as conn:
+        inspector = inspect(engine)
+        if not inspector.get_table_names():
+            Base.metadata.create_all(conn)
+        
+
+def drop_tables():
+    with engine.begin() as conn:
+        Base.metadata.drop_all(conn)
+        
+def add_post(url, header, text, theme=None, author=None, date=None):
+    with Session(engine) as session:
+        session.add(
+            ParserData(
+                url=url,
+                theme=theme,
+                header=header,
+                text=text,
+                author=author,
+                date=date
+            )
+        )
+        session.commit()
+        
+def update_post_by_url(url, data):
+    with Session(engine) as session:
+        stmt = (
+            update(ParserData)
+            .where(ParserData.url == url)
+            .values(
+                url=url,
+                theme=data["theme"],
+                header=data["header"],
+                text=data["text"],
+                author=data["author"],
+                date=None # ! Временно
+            )
+        )
+        session.execute(stmt)
+        session.commit()
+        
+def select_post_by_theme(theme: str):
+    with Session(engine) as session:
+        stmt = (
+            select(ParserData.header, ParserData.url)
+            .where(ParserData.theme == theme)
+        )
+        result = session.execute(stmt)
+        return result.all()
+    
+def delete_post_by_url(url: str):
+    with Session(engine) as session:
+        session.execute(
+            delete(ParserData)
+            .where(ParserData.url == url)
+        )
+        session.commit()
+
+def get_last_date_post():
+    pass
+
---
+++ app/database/session.py
@@ -0,0 +1,19 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+
+load_dotenv()
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_name = os.getenv("DB_NAME")
+
+
+engine = create_engine(
+    f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}/{db_name}",
+    echo=False
+)
+
+session = sessionmaker(engine, expire_on_commit=False)
